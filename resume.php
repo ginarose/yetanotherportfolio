a77c18f14b718c386ac30399844d22ba4b48f3f8
Gina Piscitelli <gina.piscitelli@ucf.edu>
2015-05-07 13:08:09 -0400

    Started adding resume content

--- resume.php
+++ resume.php
@@ -1,5 +1,51 @@
 <?php include 'header.php';?>
 
-<div class="resume-link"></div>
+<h2>Gina Piscitelli</h2>
+
+    <h3>Education</h3>
+    <h4>North Fort Myers High School</h4>
+    <ul>
+      <li>North Fort Myers, FL</li>
+      <li>Graduating Class of 2010</li>
+      <li>Digital Media track</li>
+      <li>Graduated with a 4.24 (weighted) GPA</li>
+    </ul>
+
+    <h4>University of Central Florida</h4>
+    <ul>
+      <li>Orlando, FL</li>
+      <li>Graduating Class of Fall 2014</li>
+      <li>B.A. Digital Media - Web Design </li>
+      <li>Graduated with a  3.14 (unweighted) GPA</li>
+    </ul>
+
+    <h3>Experience</h3>
+    <h4>Video Production Intern for WFTV 9 Family Connection, February 2015 - Present</h4>
+    <ul>
+      <li>Produce videos for events that are uploaded to both the web and social media outlets</li>
+      <li>Collaborate with charitable foundations to produce videos to be shown at events</li>
+      <li>Write copy for 9 Family Connection website</li>
+      <li>Provide assistance on location shoots handling studio equipment</li>
+    </ul>
+
+    <h4>Techranger at UCF Center for Distributed Learning, September 2013 - Present</h4>
+    <ul>
+      <li>Assist with design and production of posters for conventions within department</li>
+      <li>Design websites that are compliant with accessibility standards set forth</li>
+      <li>Contribute to web development projects in both team and individual settings</li>
+      <li>Use front end design skills to make websites more appealing</li>
+    </ul>
+
+    <h4>Lab Supervisor at UCF SGA Printing Lab, January 2012 - December 2014</h4>
+    <ul>
+      <li>Assisted customers with computer issues and administered software troubleshooting</li>
+      <li>Supervised employees to continuously improve the work environment</li>
+      <li>Provided friendly customer service at all times and in a professional manner</li>
+    </ul>
+
+    <h3>Skillset</h3>
+    <h3>Honors</h3>
+    <h3>References</h3>
+
 
 <?php include 'footer.php';?>
