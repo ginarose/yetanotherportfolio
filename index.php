862a5eb6b8504be6e04dc120b798d57d8fd3fb44
ginarose <ginapiscitelli@gmail.com>
2015-05-13 00:30:59 -0400

    Finished making wording changes and other little things. Going live on the index page tonight

--- index.php
+++ index.php
@@ -9,14 +9,17 @@
 <div class="pure-g about-block" id="#about">
   <div class="pure-u-1"><h2>About Me</h2></div>
   <div class="pure-u-sm-1 pure-u-md-1-3 about-picture"><img src="imgs/me.jpg" alt="Me!" class="about-picture"></div>
-  <div class="pure-u-sm-1 pure-u-md-2-3"><p class="about-text">My name is Gina Piscitelli, or Gina Rose for short. Welcome to my portfolio page! Here you'll find a <b>visual representation</b> of my work as a digital media connoisseur. Born and raised in Florida, I had the ability to attend the <b>University of Central Florida</b>, where I refined my love for all things digital and interesting. I enjoy fiddling with <b>code</b> as well as dabbling in <b>design</b> work, among other hobbies. The examples you'll find around my portfolio showcase my latest works, as well as the portfolio itself.</p></div>
+  <div class="pure-u-sm-1 pure-u-md-2-3"><p class="about-text">My name is Gina Piscitelli, or Gina Rose for short. Welcome to my portfolio page! Here you'll find a <b>visual representation</b> of my work as a digital media connoisseur. Born and raised in Florida, I had the ability to attend the <b>University of Central Florida</b>, where I refined my love for all things digital and interesting. I enjoy fiddling with <b>code</b> as well as dabbling in <b>design</b> work, among other hobbies. The examples you'll find around my portfolio showcase my latest works, as well as the portfolio itself.</p>
+    <p class="center"><a class="pure-button button-xlarge more-info-button" href="resume.php">Want to know more?</a></p>
+  </div>
+
  <div class="pure-u-sm-1 pure-u-md-1-2" class="info-box">
     <h3>Hobbies</h3>
     <h4><i class="fa info-icon fa-2x fa-lightbulb-o"></i> Learning new things</h4>
-    <p>I enjoy exploring new technology, especially if it's something in the creative field. </p>
+    <p>I enjoy exploring new technology, especially if it's something in the creative field. I enjoy picking up new programs and learning as much as I can about them on my own.</p>
     <h4><i class="fa info-icon fa-2x fa-eye"></i> Beauty blogging and nail art</h4>
     <p>Over the past few years, I have taken my love for pretty things to a whole new level: to my nailbeds. Not your typical kind of "artwork," I find <b>nail art</b> to be relaxing and allows me to be creative without having to go full-scale like painting on an 11"x14" canvas. </p>
-    <p>Along with this, I also <a href="http://www.twinklefarts.me/"><b>run a blog</b><i class="fa fa-external-link fa-small"></i></a> that has gotten me a few really interesting complimentary things. Food, nail stamping plates, and beauty products are among the things that I've reviewed at twinklefarts.com</p>
+    <p>Along with this, I also <a href="http://www.twinklefarts.me/" target="_new"><b>run a blog</b><i class="fa fa-external-link fa-small"></i></a> that has gotten me a few really interesting complimentary things. Food, nail stamping plates, and beauty products are among the things that I've reviewed at twinklefarts.com</p>
     <h4><i class="fa info-icon fa-2x fa-globe"></i> Seeing the world</h4>
     <p>It's been a goal of mine to <b>travel</b> as much as possible, and I make it a priority to do so. My next destination? <b>Italy</b>.</p>
 </div>
@@ -25,11 +28,11 @@
     <ul>
       <li><h4>Age:</h4><p>22</p></li>
       <li><h4>Height:</h4><p>5'5"</p></li>
-      <li><h4>Favorite dessert:</h4><p>Creme brulee</p></li>
       <li><h4>Favorite movie[s]:</h4><p>Billy Madison, Lion King, Toy Story</p></li>
       <li><h4>Favorite animal:</h4><p>Snakes, kittens, anything but fish</p></li>
       <li><h4>Words of wisdom:</h4><p>"Anything can happen, child. Anything can be." -Shel Silverstein</p></li>
     </ul>
+    <h3>Favorite Programs</h3>
 </div>
   
  </div>
